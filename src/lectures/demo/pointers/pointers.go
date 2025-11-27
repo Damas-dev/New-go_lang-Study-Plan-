bbdc709d53e1f72ccee5b6910c4b2572bc61397f
Damas Joseph <damasmtete10@gmail.com>
2025-11-27 21:15:04 +0300

    added: pointers Demo

--- src/lectures/demo/pointers/pointers.go
+++ src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
+type Counter struct {
+	hits int
+}
+
+func incremet(counter *Counter) {
+	counter.hits += 1
+	fmt.Println("counter:", counter.hits)
+}
+
+func replaceString(old *string, new string, count *Counter) {
+	*old = new
+	incremet(count)
+}
+
 func main() {
+
+	count := Counter{}
+
+	hello := "hello"
+	world := "world!"
+	name := "Damas"
+
+	fmt.Println(hello, world)
+
+	replaceString(&hello, "hi", &count)
+
+	fmt.Println(hello, world)
+
+	phrase := []string{hello, world}
+
+	replaceString(&phrase[1], name, &count)
+
+	fmt.Println(phrase)
 
 }
