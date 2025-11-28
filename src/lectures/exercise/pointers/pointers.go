c3feb68de74b8799c25d29fd85274baa592267ae
Damas Joseph <damasmtete10@gmail.com>
2025-11-28 23:41:49 +0300

    added: pointers exercise

--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -18,6 +18,69 @@ package main
 
 import "fmt"
 
+// * Create a structure to store items and their security tag state
+//   - Security tags have two states: active (true) and inactive (false)
+const (
+	active   = true
+	inactive = false
+)
+
+type Item struct {
+	name  string
+	state bool
+}
+
+// * Create functions to activate and deactivate security tags using pointers
+func activate(i *Item) {
+	i.state = active
+
+}
+
+func deactivate(i *Item) {
+	i.state = inactive
+
+}
+
+// * Create a checkout() function which can deactivate all tags in a slice
+func checkout(i *[]Item) {
+	for _, v := range *i {
+		v.state = inactive
+		fmt.Println(v.name, ":inactive")
+	}
+
+}
+
 func main() {
+
+	//  - Create at least 4 items, all with active security tags
+
+	car1 := Item{"car1", active}
+	car2 := Item{"car2", active}
+	car3 := Item{"car3", active}
+	car4 := Item{"car4", active}
+	car5 := Item{"car5", active}
+
+	//  - Store them in a slice or array
+	items := []Item{
+		car1,
+		car2,
+		car3,
+		car4,
+		car5,
+	}
+
+	fmt.Println(items)
+
+	//  - Deactivate any one security tag in the array/slice
+	deactivate(&car5)
+	fmt.Println(items)
+
+	//  - Call the checkout() function to deactivate all tags
+	checkout(&items)
+	fmt.Println(items)
+
+	activate(&car3)
+
+	fmt.Println(items)
 
 }
