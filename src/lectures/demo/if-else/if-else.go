ef52193b85539b4885de8b4b8bbb7a6b5c2fdcd8
Damas Joseph <damasmtete10@gmail.com>
2025-12-08 15:45:02 +0300

    added: demo--> if-else.go

--- src/lectures/demo/if-else/if-else.go
+++ src/lectures/demo/if-else/if-else.go
@@ -8,5 +8,27 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	//!q = quiz
+
+	q1, q2, q3 := 7, 8, 9
+
+	const passMark float32 = 7
+
+	if q1 > q2 && q1 > q3 {
+		fmt.Printf("you scored the highest in quiz1: %v\n", q1)
+	} else if q2 > q3 {
+		fmt.Printf("you scored the highest in quiz2: %v\n", q2)
+	} else {
+		fmt.Printf("you scored the highest in quiz3: %v\n", q3)
+
+	}
+
+	// cheak if the student has passed or not
+
+	if average(q1, q2, q3) >= passMark {
+		fmt.Println("status: PASS")
+	} else {
+		fmt.Println("status: FAIL")
+	}
 
 }
