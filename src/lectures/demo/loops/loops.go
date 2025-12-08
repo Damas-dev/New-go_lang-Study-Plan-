75950ce9c0686fb33c1afe8077d130a3985f2dc8
Damas Joseph <damasmtete10@gmail.com>
2025-12-08 21:21:27 +0300

    added: demo--> loops.go

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -4,4 +4,17 @@ import "fmt"
 
 func main() {
 
+	sum := 0
+
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("Sum:", sum)
+	}
+
+	for sum >= 10 {
+		fmt.Println("<Sum:", sum)
+		sum -= 5
+
+	}
+
 }
