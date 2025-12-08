6270d38cbccfafbce723c45b1824c0bb1a290c5a
Damas Joseph <damasmtete10@gmail.com>
2025-12-08 17:03:05 +0300

    added: demo--> switch.go

--- src/lectures/demo/switch/switch.go
+++ src/lectures/demo/switch/switch.go
@@ -13,5 +13,30 @@ const (
 )
 
 func main() {
+	switch p := price(); {
+	case p < 2:
+		fmt.Println("Cheap item")
 
+		fallthrough // allow the exechution of the next case
+	case p < 10:
+		fmt.Println("Modelete price")
+	case p < 100:
+		fmt.Println("Expenceve item")
+	default:
+		fmt.Println("Very expenceve item")
+
+	}
+
+	ticket := Economy
+
+	switch ticket {
+	case Economy:
+		fmt.Println("Economy sitting")
+	case Business:
+		fmt.Println("Business sitting")
+	case FirstClass:
+		fmt.Println("first Class sitting")
+	default:
+		fmt.Println("other sittings")
+	}
 }
