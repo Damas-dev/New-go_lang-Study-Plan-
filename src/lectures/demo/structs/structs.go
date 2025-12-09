cf4c67142923a2fd2ff403824e9e57aa13c85e8e
Damas Joseph <damasmtete10@gmail.com>
2025-12-09 13:35:59 +0300

    added: demo--> struct.go

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,6 +2,50 @@ package main
 
 import "fmt"
 
+type Student struct {
+	name     string
+	id       int
+	hasAbook bool
+}
+type Libary struct {
+	inLibary Student
+}
+
 func main() {
+
+	damas := Student{"Damas", 1, false}
+
+	fmt.Println(damas)
+
+	var (
+		jose = Student{"Joseph", 2, false}
+		win  = Student{"Winfrida", 3, false}
+	)
+
+	fmt.Println(jose, win)
+
+	jose.hasAbook = true
+	win.hasAbook = true
+
+	if jose.hasAbook {
+		fmt.Println(jose.name, "has a book form the library")
+	} else {
+		fmt.Println(jose.name, "has no book form the library")
+
+	}
+	if win.hasAbook {
+		fmt.Println(win.name, "has a book form the library")
+	} else {
+		fmt.Println(win.name, "has no book form the library")
+	}
+	if damas.hasAbook {
+		fmt.Println(damas.name, "has a book form the library")
+	} else {
+		fmt.Println(damas.name, "has no book form the library")
+	}
+
+	lib1 := Libary{damas}
+
+	fmt.Println(lib1.inLibary.name, "is in libray1")
 
 }
