9f11bbca7c4759f6cc492d19852f846657d382eb
Damas Joseph <damasmtete10@gmail.com>
2025-12-11 10:18:27 +0300

    added: exercise--> struct.go

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -1,63 +1,43 @@
-//--Summary:
-//  Create a program to calculate the area and perimeter
-//  of a rectangle.
-//
-//--Requirements:
-//* Create a rectangle structure containing its coordinates
-//* Using functions, calculate the area and perimeter of a rectangle,
-//  - Print the results to the terminal
-//  - The functions must use the rectangle structure as the function parameter
-//* After performing the above requirements, double the size
-//  of the existing rectangle and repeat the calculations
-//  - Print the new results to the terminal
-//
-//--Notes:
-//* The area of a rectangle is length*width
-//* The perimeter of a rectangle is the sum of the lengths of all sides
-
 package main
 
 import "fmt"
 
-// ~ Create a rectangle structure containing its coordinates
-type Rectangle struct {
-	lenght float64
-	width  float64
+type Codinates struct {
+	x, y int
 }
 
-// ~ Using functions, calculate the area and perimeter of a rectangle,
-//   - Print the results to the terminal
-//   - The functions must use the rectangle structure as the function parameter
-func rectangleArea(rec Rectangle) float64 {
-	return (rec.lenght * rec.width)
+type rectPoints struct {
+	a, b Codinates
 }
 
-func rectanglePerimeter(rec Rectangle) float64 {
-	return (rec.lenght*2 + rec.width*2)
+func width(points rectPoints) int {
+	return points.b.y - points.a.y
 }
 
-//* After performing the above requirements, double the size
-//  of the existing rectangle and repeat the calculations
-//  - Print the new results to the terminal
+func lenght(points rectPoints) int {
+	return points.b.x - points.a.x
+}
 
-func doubleTheValue(rec Rectangle) (float64, float64) {
-	return rec.lenght * 2, rec.width * 2
+func area(points rectPoints) int {
+	return width(points) * lenght(points)
+}
+
+func perimeter(points rectPoints) int {
+	return width(points)*2 + lenght(points)*2
+}
+
+func printInfo(points rectPoints) {
+	fmt.Println("===== Rectangle Area and Perimeter =====")
+	fmt.Println("--- given point a(x,y) `top right` & point b(x,y) `bottom left` ---")
+	fmt.Println("given", "a(", points.a.x, ",", points.a.y, ")  &  b(", points.b.x, ",", points.b.y, ")")
+
+	fmt.Println("Area:", area(points))
+	fmt.Println("Perimeter:", perimeter(points))
 
 }
 
 func main() {
+	codPoints := rectPoints{a: Codinates{0, 7}, b: Codinates{4, 17}}
 
-	rectangle1 := Rectangle{3, 4}
-
-	fmt.Println("Rectangle1 Area:", rectangleArea(rectangle1))
-
-	fmt.Println("Rectangle1 Perimeter:", rectanglePerimeter(rectangle1))
-
-	rectangle1.lenght, rectangle1.width = doubleTheValue(rectangle1)
-	fmt.Println(rectangle1)
-
-	fmt.Println("Rectangle1 Area:", rectangleArea(rectangle1))
-
-	fmt.Println("Rectangle1 Perimeter:", rectanglePerimeter(rectangle1))
-
+	printInfo(codPoints)
 }
